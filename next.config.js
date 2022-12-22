e27502f2da26f1fa757ab2aa0b52275f57a40b79
faruks69 <71905990+faruks69@users.noreply.github.com>
2022-12-22 21:58:45 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
+    TMDB_API_KEY: process.env.79c66d373fd1a36801c5746233e609c9,
   },
   images: {
     domains: ['image.tmdb.org'],
