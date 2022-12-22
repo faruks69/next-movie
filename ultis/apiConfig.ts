6283dc6860868ef1dfa7231f77c242e0c622b63a
faruks69 <71905990+faruks69@users.noreply.github.com>
2022-12-22 22:21:16 +0600

    Update apiConfig.ts

--- ultis/apiConfig.ts
+++ ultis/apiConfig.ts
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: 'https://api.themoviedb.org/3',
+  baseUrl: 'https://api.themoviedb.org/3/movie/550?api_key=79c66d373fd1a36801c5746233e609c9',
   apiKey: process.env.TMDB_API_KEY,
 };
 
